af910d3e5aa21b7f591845d64f1f55ccfa159aed
AryanBagane <94216235+AryanBagane@users.noreply.github.com>
2023-12-22 14:04:36 +0530

    Add files via upload

---
+++ clean.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Replace 'your_file.csv' with the actual name of your CSV file
+file_path = '/home/user/Desktop/Data Analytics Project/RS_Session_258_AU_923_3.csv'
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(file_path)
+
+# Remove last row
+df.drop(df.index[-1], inplace=True)
+
+# Replace null values with zero for all columns
+df.fillna(0, inplace=True)
+
+# Save the modified DataFrame to a new CSV file
+output_file_path = '/home/user/Desktop/Data Analytics Project/final_data.csv'
+df.to_csv(output_file_path, index=False)
+
+# Print the DataFrame to verify the changes
+print(df)
+print(output_file_path, "file created successfully.")
